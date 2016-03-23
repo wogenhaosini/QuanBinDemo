8e004dc76c72b28803f34a989b2815cbef8eaad2
wogenhaosini <bin.quan.lm@gmail.com>
2016-03-23 15:07:41 +0800

    上传

--- app/src/main/java/com/quan/demo/MainActivity.java
+++ app/src/main/java/com/quan/demo/MainActivity.java
@@ -1,6 +1,7 @@
 package com.quan.demo;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.design.widget.CoordinatorLayout;
@@ -10,8 +11,12 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.MenuItem;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.quan.demo.cart.DiscoveryDetailActivity;
 
 /**
@@ -21,6 +26,11 @@ public class MainActivity extends AppCompatActivity {
 
 
     private DrawerLayout mDrawerLayout;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient mClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,12 +53,11 @@ public class MainActivity extends AppCompatActivity {
         Toolbar mToolbar = (Toolbar) findViewById(R.id.toolBar);
         setSupportActionBar(mToolbar);
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
         CoordinatorLayout coord = (CoordinatorLayout) findViewById(R.id.coord);
         CollapsingToolbarLayout toolBarLayouy = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayouy.setTitle("测试");
-        //mDrawerLayout.setScrimColor(getResources().getColor(R.color.colorPrimary));
-       // mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.LEFT);
+        //mDrawerLayout.setScrimColor(Color.TRANSPARENT);
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, Gravity.LEFT);
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         //mToolbar.setTitle(R.string.nav_titile); //標題
         //mToolbar.setSubtitle(R.string.nav_subTitle); //副標題
@@ -76,14 +85,14 @@ public class MainActivity extends AppCompatActivity {
         //getSupportActionBar().setDisplayShowHomeEnabled(false);//使左上角图标 是否展示
         //getSupportActionBar().setIcon(R.drawable.ic_launcher); //設置左上角的icon
 
-        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.dakai, R.string.guanbi) ;
+        ActionBarDrawerToggle mActionBarDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, mToolbar, R.string.dakai, R.string.guanbi);
         mActionBarDrawerToggle.syncState(); //設置為三道杠
         mDrawerLayout.addDrawerListener(mActionBarDrawerToggle); //不設置監聽 也不會動
         /**
          * setItemBackgroundResource(int)：给menu设置背景资源，对应的属性app:itemBackground
          * setItemIconTintList(ColorStateList)：给menu的icon设置颜色，对应的属性app:itemIconTint
          * setItemTextColor(ColorStateList)：给menu的item设置字体颜色，对应的属性app:itemTextColor
-           navigationView.setItemIconTintList(new ColorStateList(){});
+         navigationView.setItemIconTintList(new ColorStateList(){});
          */
         navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -94,6 +103,9 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
@@ -103,5 +115,45 @@ public class MainActivity extends AppCompatActivity {
             mDrawerLayout.openDrawer(GravityCompat.START);
         }
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        mClient.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.quan.demo/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(mClient, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "Main Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app deep link URI is correct.
+                Uri.parse("android-app://com.quan.demo/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(mClient, viewAction);
+        mClient.disconnect();
     }
 }
